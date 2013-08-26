08fe2c5e2c77ee93221ff3c4680213b64856e016
orbisnull <zen4dev@gmail.com>
2013-08-27 01:56:37 +0900

    wirking on nginx style locations

--- src/Kindrouter/Router.php
+++ src/Kindrouter/Router.php
@@ -21,14 +21,17 @@ class Router
             throw new Exception('Bad callback function');
         }
 
+        $priority = 9;
         if (($pos=(strpos($pattern, self::DELIMITER)))!==false) {
             $strSc = substr($pattern, 0 ,$pos);
             switch ($strSc) {
                 case self::SC_FULL:
                     $scheme = self::SC_FULL;
+                    $priority = 0;
                     break;
                 case self::SC_REG:
                     $scheme = self::SC_REG;
+                    $priority = 6;
                     break;
                 case self::SC_STR:
                     $scheme = self::SC_STR;
@@ -40,7 +43,12 @@ class Router
             $scheme = self::SC_STR;
         }
 
-        $urlParams = ['pattern'=>$pattern, 'scheme'=>$scheme, 'callback' => $callback];
+        if ($scheme === self::SC_STR && $pattern[0] === '^') {
+            $priority = 3;
+            $pattern = substr($pattern, 1);
+        }
+
+        $urlParams = ['pattern'=>$pattern, 'scheme'=>$scheme, 'callback' => $callback, 'priority' => $priority];
         if (!is_null($args)) {
             $urlParams['args'] = $args;
         }
@@ -82,6 +90,20 @@ class Router
         return $url;
     }
 
+    //если 0 - сразу
+    // если 3  - наибольший
+    // если 6  - по порядку
+    // если 9 - наибольший
+    public function chooseUrl(array $urls)
+    {
+        ksort($urls);
+        foreach ($urls as $priority=>$urlsIntrList) {
+            ///
+        }
+
+    }
+
+    //TODO add prefix to str ^ - stop prefix search on this prefix
     public function run()
     {
         if ($this->isRun) { return; }
@@ -90,23 +112,54 @@ class Router
         $methods  = [self::METHOD_ALL, $_SERVER['REQUEST_METHOD']];
         $strUrl = $this->getUrlNormal();
 
+
+        $suitableUrls = [];
+        $topPr = 99;
         foreach ($methods as &$method) {
             $currUrls = empty($this->urls[$method]) ? [] : $this->urls[$method];
-            foreach ($currUrls as &$urlData) {
+            foreach ($currUrls as $urlData) {
+                if ($urlData['priority'] > $topPr) {
+                    continue;
+                }
                 if ($this->testUrl($strUrl, $urlData['pattern'], $urlData['scheme'])) {
-                    if (isset($urlData['args'])) {
-                        return call_user_func_array($urlData['callback'], $urlData['args']);
-                    } else {
-                        return call_user_func($urlData['callback']);
+                    $topPr = $urlData['priority'];
+                    if ($urlData['priority'] === 0) {
+                        $suitableUrls = [$urlData['priority'] => [$urlData]];
+                        break;
                     }
+                    $suitableUrls[$urlData['priority']][] = $urlData;
                 }
             }
+        }
+
+        //TODO more good 404
+        $urlData = $this->chooseUrl($suitableUrls);
+        if (empty($urlData)) {
+            echo $this->exception404();
+            return;
+        }
+
+        if (isset($urlData['args'])) {
+            return call_user_func_array($urlData['callback'], $urlData['args']);
+        } else {
+            return call_user_func($urlData['callback']);
         }
     }
 
     function __invoke()
     {
         return $this->run();
+    }
+
+    public function exception404()
+    {
+        if (headers_sent()) {
+            throw new \LogicException('Headers already send, url no found');
+        }
+        header($_SERVER["SERVER_PROTOCOL"]." 404 Not Found");
+        header("Status: 404 Not Found");
+        $_SERVER['REDIRECT_STATUS'] = 404;
+        return "<h1>Not Found</h1>";
     }
 
 
