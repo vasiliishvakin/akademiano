b89ab6cde817488cf823dfdbdf77100db5732eee
orbisnull <zen4dev@gmail.com>
2014-03-19 20:42:38 +0900

    rm old composer settings

--- public/index.php
+++ public/index.php
@@ -7,17 +7,9 @@ define('ROOT_DIR', realpath(__DIR__ . '/..'));
 define('PUBLIC_DIR', ROOT_DIR . '/public');
 define('DATA_DIR', ROOT_DIR . '/data');
 
-set_include_path(
-ROOT_DIR . '/controller'
-. PATH_SEPARATOR  .
-get_include_path());
-
 $loader = include_once ROOT_DIR . "/vendor/autoload.php";
-$loader->setUseIncludePath(true);
 
-use DeltaCore\Application;
-
-$app = new Application();
+$app = new \DeltaCore\Application();
 $app->setLoader($loader);
 
 $app->run();
