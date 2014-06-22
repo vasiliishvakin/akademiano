fa5cedd0162fe34198ad8baa7564056d523a672e
orbisnull <zen4dev@gmail.com>
2014-06-23 04:20:44 +0900

    fix init user manager (add groupManager)

--- src/User/config/resources.php
+++ src/User/config/resources.php
@@ -3,13 +3,11 @@
  * User: Vasiliy Shvakin (orbisnull) zen4dev@gmail.com
  */
 return [
-    'userManager'      => function ($c) {
-            $userManager = new \User\Model\UserManager();
-            $userManager->setSession($c['sessions']);
-            return $userManager;
-        },
-    'groupManager'      => function ($c) {
-        $userManager = new \User\Model\GroupManager();
+    'userManager' => function ($c) {
+        $userManager = new \User\Model\UserManager();
+        $userManager->setSession($c['sessions']);
+        $gm = $c["groupManager"];
+        $userManager->setGroupManager($gm);
         return $userManager;
-    }
+    },
 ];
