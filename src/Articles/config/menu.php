037cc441b776251391afba2aec659fbda131c58f
orbisnull <zen4dev@gmail.com>
2014-08-30 02:18:59 +0900

    add menu config

---
+++ src/Articles/config/menu.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    "main" => [
+        ["text" => "Статьи", "link" => "/articles"],
+    ],
+    "admin" => [
+        ["text" => "Статьи", "link" => "/admin/articles"],
+    ],
+];
