9ce9eb5e2e8aac64da8c9946297dbd0fb21a58ba
orbisnull <zen4dev@gmail.com>
2015-04-03 19:28:32 +0900

    get CategoryManager from root DI (set it in main resource.php)

--- src/Articles/config/resources.php
+++ src/Articles/config/resources.php
@@ -5,16 +5,9 @@
 return [
     "ArticlesManager" => function ($c) {
             $nm = new \Articles\Model\ArticlesManager();
-            $df = $c["directoryFactory"];
-            $cm = $df->getManager("article_categories");
-            $nm->setCategoryManager($cm);
+            $nm->setCategoryManager($c["articleCategoriesManager"]);
             $fm = $c["fileManager"];
             $nm->setFileManager($fm);
             return $nm;
-    },
-
-    'articleCategoriesManager' => function ($c) {
-        /** @var \DeltaCore\Application $c */
-        return $c["directoryFactory"]->getManager("article_categories");
     },
 ];
