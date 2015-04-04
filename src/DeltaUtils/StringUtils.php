cb57acc71baddeff38b90fcc1e796549e3d6d7b1
orbisnull <zen4dev@gmail.com>
2015-04-05 05:04:06 +0900

    add method camelCaseToLowDash

--- src/DeltaUtils/StringUtils.php
+++ src/DeltaUtils/StringUtils.php
@@ -99,6 +99,16 @@ class StringUtils
         return $string;
     }
 
+    public static function camelCaseToLowDash($input)
+    {
+        preg_match_all('!([A-Z][A-Z0-9]*(?=$|[A-Z][a-z0-9])|[A-Za-z][a-z0-9]+)!', $input, $matches);
+        $ret = $matches[0];
+        foreach ($ret as &$match) {
+            $match = $match == strtoupper($match) ? strtolower($match) : lcfirst($match);
+        }
+        return implode('_', $ret);
+    }
+
     public static function toIdStr9($id)
     {
         return sprintf("id%09s", (integer) $id);
