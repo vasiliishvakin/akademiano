d623e8e7c20c63867d1d25f293045e8888c9ef55
orbisnull <zen4dev@gmail.com>
2015-10-30 13:14:58 +0800

    add work with assets

--- config/global.config.php
+++ config/global.config.php
@@ -1,6 +1,11 @@
 <?php
 
 return [
+    "database" => [
+        "default" => [
+            "name" => "deltaskeleton",
+        ]
+    ],
     'view' => [
         'adapter' => 'twig',
         'templateDirs' => [
@@ -14,10 +19,29 @@ return [
         ],
         "extensions" => [
             "Twig_Extension_Debug",
+//            "Assetic\\Extension\\Twig\\AsseticExtension",
+            "DeltaTwigExt\\AssetExtension",
         ],
+        "filters"      => [
+            "cropBySpace" => [["\\DeltaUtils\\StringUtils", "cropBySpace"], ['pre_escape' => 'html']],
+            "urlToTag" => [["\\DeltaUtils\\StringUtils", "urlToTag"], ['pre_escape' => 'html', 'is_safe' => array('html')]],
+            "nl2p"     => [["\\DeltaUtils\\StringUtils", "nl2p"], ['pre_escape' => 'html', 'is_safe' => array('html')]],
+            "cutStr"     => [["\\DeltaUtils\\StringUtils", "cutStr"], ['pre_escape' => 'html', 'is_safe' => array('html')]],
+            "nl2Array"     => [["\\DeltaUtils\\StringUtils", "nl2Array"], []],
+            "idStr"     => [["\\DeltaUtils\\StringUtils", "toIdStr9"], []],
+        ],
+    ],
+    "init"    => [
+        "setLocale" => function($c) {
+            setlocale(LC_ALL, 'ru_RU.UTF-8');
+            setlocale(LC_NUMERIC, "en_US.UTF-8");
+            locale_set_default('ru');
+        }
     ],
     "modules" => [
         "DeltaSkeletonModule",
-    ]
+        "DeltaDb",
+        "Pages"
+    ],
 
 ];
--- public/index.php
+++ public/index.php
@@ -3,8 +3,9 @@
 error_reporting(E_ALL);
 ini_set('display_errors', true);
 
-define('ROOT_DIR', realpath(__DIR__ . '/..'));
+define('ROOT_DIR', realpath(__DIR__ . '/..')); // надо исключить realpath из кода и вынести в конфиг
 define('PUBLIC_DIR', ROOT_DIR . '/public');
+define('VENDOR_DIR', ROOT_DIR . '/vendor');
 define('DATA_DIR', ROOT_DIR . '/data');
 
 $loader = include_once ROOT_DIR . "/vendor/autoload.php";
