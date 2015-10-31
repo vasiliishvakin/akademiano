f441a27793e4b715c0f7efe54fd548ea33cfc7fd
orbisnull <zen4dev@gmail.com>
2015-10-31 20:44:15 +0800

    add pages module

--- config/global.config.php
+++ config/global.config.php
@@ -21,18 +21,30 @@ return [
             "Twig_Extension_Debug",
 //            "Assetic\\Extension\\Twig\\AsseticExtension",
             "DeltaTwigExt\\AssetExtension",
+            "DeltaTwigExt\\UrlExtension"
         ],
-        "filters"      => [
+        "filters" => [
             "cropBySpace" => [["\\DeltaUtils\\StringUtils", "cropBySpace"], ['pre_escape' => 'html']],
-            "urlToTag" => [["\\DeltaUtils\\StringUtils", "urlToTag"], ['pre_escape' => 'html', 'is_safe' => array('html')]],
-            "nl2p"     => [["\\DeltaUtils\\StringUtils", "nl2p"], ['pre_escape' => 'html', 'is_safe' => array('html')]],
-            "cutStr"     => [["\\DeltaUtils\\StringUtils", "cutStr"], ['pre_escape' => 'html', 'is_safe' => array('html')]],
-            "nl2Array"     => [["\\DeltaUtils\\StringUtils", "nl2Array"], []],
-            "idStr"     => [["\\DeltaUtils\\StringUtils", "toIdStr9"], []],
+            "urlToTag" => [
+                ["\\DeltaUtils\\StringUtils", "urlToTag"],
+                ['pre_escape' => 'html', 'is_safe' => array('html')]
+            ],
+            "nl2p" => [["\\DeltaUtils\\StringUtils", "nl2p"], ['pre_escape' => 'html', 'is_safe' => array('html')]],
+            "cutStr" => [["\\DeltaUtils\\StringUtils", "cutStr"], ['pre_escape' => 'html', 'is_safe' => array('html')]],
+            "nl2Array" => [["\\DeltaUtils\\StringUtils", "nl2Array"], []],
+            "idStr" => [["\\DeltaUtils\\StringUtils", "toIdStr9"], []],
         ],
+        "urlExtension" => [
+            "routeGenerator" => [
+                \DeltaCore\Config::DYN_CONF => function ($c) {
+                    $router = $c["router"];
+                    return [$router, "getUrl"];
+                }
+            ]
+        ]
     ],
-    "init"    => [
-        "setLocale" => function($c) {
+    "init" => [
+        "setLocale" => function ($c) {
             setlocale(LC_ALL, 'ru_RU.UTF-8');
             setlocale(LC_NUMERIC, "en_US.UTF-8");
             locale_set_default('ru');
