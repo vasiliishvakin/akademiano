e7ae881a3318ee675256697f82af4326f85ac673
orbisnull <zen4dev@gmail.com>
2015-12-13 01:58:38 +0800

    add new image templates

--- src/Image/config/config.php
+++ src/Image/config/config.php
@@ -23,9 +23,26 @@ return [
                 "clear",
                 "optimize"
             ],
-            "origin" => [
+            "250x250" => [
+                "resizeAndCrop" => [250, 250],
                 "addWatermark" => function (\DeltaCore\Config $c) {
                     return $c->getOrThrow("watermark");
+                },
+                "clear",
+                "optimize"
+            ],
+            "400x300" => [
+                "resizeAndCrop" => [400, 300],
+                "addWatermark" => function (\DeltaCore\Config $c) {
+                    return $c->getOrThrow("watermark");
+                },
+                "clear",
+                "optimize"
+            ],
+            "origin" => [
+                "addWatermark" => function (\DeltaCore\Config $c) {
+                    $text = $c->getOrThrow("watermark")->getText();
+                    return new \Image\Model\Watermark(["text" => $text, "mode" => \Image\Model\Watermark::MODE_TEXT_MOSAIC]);
                 },
                 "clear",
                 "optimize" => [95],
