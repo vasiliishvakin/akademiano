b82a31db7be30e71ef7a6b6be9b7cd4cfd149727
orbisnull <zen4dev@gmail.com>
2016-01-23 13:44:12 +0800

    update work with files, (upload, view article).

---
+++ src/Articles/migrations/20160118062032_articles_date_to_timestamp_without_zone_pg.php
@@ -0,0 +1,35 @@
+<?php
+
+use Phinx\Migration\AbstractMigration;
+
+class ArticlesDateToTimestampWithoutZonePg extends AbstractMigration
+{
+    /**
+     * Change Method.
+     *
+     * Write your reversible migrations using this method.
+     *
+     * More information on writing migrations is available here:
+     * http://docs.phinx.org/en/latest/migrations.html#the-abstractmigration-class
+     *
+     * The following commands can be used in this method and Phinx will
+     * automatically reverse them when rolling back:
+     *
+     *    createTable
+     *    renameTable
+     *    addColumn
+     *    renameColumn
+     *    addIndex
+     *    addForeignKey
+     *
+     * Remember to call "create()" or "update()" and NOT "save()" when working
+     * with the Table class.
+     */
+    public function change()
+    {
+        $table = $this->table("articles");
+        $table->changeColumn("created", "timestamp", ["timezone" => false, "default" => "CURRENT_TIMESTAMP"])
+            ->changeColumn("changed", "timestamp", ["timezone" => false, "default" => "CURRENT_TIMESTAMP"])
+            ->update();
+    }
+}
