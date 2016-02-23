f22ce1f47c247437505f634e1e4b7e1a14fa8cd8
orbisnull <zen4dev@gmail.com>
2016-02-23 11:41:07 +0800

    fix explode when filetype (mime) not defained in db

--- src/Attach/migrations/20160116131641_attach_type_explode_to_two_fields.php
+++ src/Attach/migrations/20160116131641_attach_type_explode_to_two_fields.php
@@ -7,9 +7,21 @@ class AttachTypeExplodeToTwoFields extends AbstractMigration
 
     public function up()
     {
+        if (!defined("ROOT_DIR")) {
+            define('ROOT_DIR', realpath(__DIR__ . '/..'));
+        }
+
         $files = $this->table("files");
         $files->addColumn("sub_type", "text", ['null' => true]);
         $files->update();
+
+        $sql = "select * from files where TYPE  is NULL";
+        $data = $this->query($sql);
+        foreach ($data as $row) {
+            $path = (mb_substr($row["path"], 0, 1) !== "/") ? ROOT_DIR . DIRECTORY_SEPARATOR . $row["path"] : $row["path"];
+            $type = \DeltaUtils\FileSystem::getFileType($path);
+            $this->execute("update files set type='{$type}' where id=" . $row["id"]);
+        }
 
         $sql = "update files set sub_type=split_part(type, '/', 2), type=split_part(type, '/', 1)";
         $this->execute($sql);
