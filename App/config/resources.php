64e4d548d6ed3c12de50d4de5d66ae68d94b0f56
orbisnull <zen4dev@gmail.com>
2016-03-13 19:06:05 +0800

    add configurable environment (and use it in attach)

--- App/config/resources.php
+++ App/config/resources.php
@@ -1,7 +1,14 @@
 <?php
-/**
- * User: Vasiliy Shvakin (orbisnull) zen4dev@gmail.com
- */
 return [
-
+    "environment" => function($c) {
+        /** @var \DeltaCore\Application $c */
+        $config = $c->getConfig("environment", null);
+        $env = new \HttpWarp\Environment();
+        if ($config instanceof \DeltaCore\Config) {
+            $config->getAndCall("serverName", [$env, "setServerName"]);
+            $config->getAndCall("port", [$env, "setPort"]);
+            $config->getAndCall("https", [$env, "setHttps"]);
+        }
+       return $env;
+    }
 ];
