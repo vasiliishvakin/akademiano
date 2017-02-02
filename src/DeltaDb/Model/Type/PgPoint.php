26a9e41910d50323ba42a91109283c28d92d18d7
orbisnull <zen4dev@gmail.com>
2017-02-03 00:12:17 +0800

    fix work with new DeltaCore (moved interface)

--- src/DeltaDb/Model/Type/PgPoint.php
+++ src/DeltaDb/Model/Type/PgPoint.php
@@ -6,7 +6,7 @@
 namespace DeltaDb\Model\Type;
 
 
-use DeltaCore\Prototype\ArrayableInterface;
+use DeltaUtils\Object\Prototype\ArrayableInterface;
 use DeltaDb\Model\Type\ReservableInterface;
 use DeltaUtils\Object\Prototype\StringableInterface;
 use DeltaUtils\StringUtils;
