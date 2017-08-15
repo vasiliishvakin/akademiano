6083d05db7f4b66a5bb24373b18b50fbbc438fdb
mrdatamapper <mrdatamapper@gmail.com>
2017-08-15 22:18:32 +0800

    Init Commit

---
+++ src/Container.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Akademiano\DI;
+
+
+use Pimple\Exception\UnknownIdentifierException;
+use Akademiano\Config\ConfigurableInterface;
+
+class Container extends \Pimple\Container
+{
+    protected $values = [];
+
+    public function lazyGet($id)
+    {
+        return function() use ($id) {
+            return $this[$id];
+        };
+    }
+
+    protected function prepare($value)
+    {
+        if ($value instanceof ConfigurableInterface) {
+            $value->setConfig($this["config"]);
+        }
+        return $value;
+    }
+
+    public function offsetGet($id)
+    {
+        if (!$this->offsetExists($id)) {
+            throw new UnknownIdentifierException($id);
+        }
+        if (!isset($this->values[$id])) {
+            $this->values[$id] = $this->prepare(parent::offsetGet($id));
+        }
+        return $this->values[$id];
+    }
+
+    public function offsetUnset($id)
+    {
+        unset($this->values[$id]);
+        parent::offsetUnset($id);;
+    }
+}
