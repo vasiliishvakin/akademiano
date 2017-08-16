f5ce6e9bcdb7bf04d82770870e7f34e2d265161c
mrdatamapper <mrdatamapper@gmail.com>
2017-08-16 13:32:57 +0800

    migrations order fix, fix fields in worker

--- src/Model/FilesWorker.php
+++ src/Model/FilesWorker.php
@@ -11,5 +11,5 @@ class FilesWorker extends PostgresWorker
     const TABLE_ID_INC = 1;
     const TABLE_ID = 13;
     const TABLE_NAME = "files_linked";
-    const EXPAND_FIELDS = ["title", "type", "sub_type", "path", "position", "size", "mime_type", "entity"];
+    const EXPAND_FIELDS = ["title", "type", "sub_type", "path", "position", "size", "mime_type"];
 }
