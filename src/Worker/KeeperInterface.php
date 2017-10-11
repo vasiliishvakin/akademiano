ea375408a565dfa6e5d5e354263ccbff0f1d728e
mrdatamapper <mrdatamapper@gmail.com>
2017-10-11 17:18:28 +0800

    add to KeeperInterface constants for info command support

--- src/Worker/KeeperInterface.php
+++ src/Worker/KeeperInterface.php
@@ -6,6 +6,9 @@ namespace Akademiano\EntityOperator\Worker;
 
 interface KeeperInterface
 {
+    const WORKER_INFO_TABLE = 'table';
+    const WORKER_INFO_FIELDS = 'fields';
+
     public function get($id);
 
     public function save(array $data, $isExisting);
