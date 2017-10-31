d868be19602f82f109d1904a281b156d2b18e464
mrdatamapper <mrdatamapper@gmail.com>
2017-10-31 15:37:39 +0800

    logger may be not set, no error on log action

--- src/Parts/LoggerTrait.php
+++ src/Parts/LoggerTrait.php
@@ -11,7 +11,7 @@ trait LoggerTrait
     /** @var  LoggerInterface */
     protected $logger;
 
-    public function getLogger(): LoggerInterface
+    public function getLogger(): ?LoggerInterface
     {
         return $this->logger;
     }
@@ -26,6 +26,9 @@ trait LoggerTrait
 
     public function log($level, $message, array $context = []):void
     {
-        $this->getLogger()->log($level, $message, $context);
+        $logger = $this->getLogger();
+        if ($logger instanceof LoggerInterface) {
+            $this->getLogger()->log($level, $message, $context);
+        }
     }
 }
