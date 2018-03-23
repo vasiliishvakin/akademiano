f2f853989b1cf9dc080c748789734e4b894f2b38
mrdatamapper <mrdatamapper@gmail.com>
2018-03-23 17:59:36 +0800

    add new templates

--- src/config/templates.php
+++ src/config/templates.php
@@ -26,9 +26,60 @@ return [
         $image->resize($long, $long, false);
         return $image->save($newPath, $format, 80);
     },
+    'l300' => function (\PHPixie\Image\Drivers\Driver\Resource $image, $newPath, $format) {
+        $long = 300;
+        $image->resize($long, $long, false);
+        return $image->save($newPath, $format, 80);
+    },
+    'l400' => function (\PHPixie\Image\Drivers\Driver\Resource $image, $newPath, $format) {
+        $long = 400;
+        $image->resize($long, $long, false);
+        return $image->save($newPath, $format, 80);
+    },
+    'l500' => function (\PHPixie\Image\Drivers\Driver\Resource $image, $newPath, $format) {
+        $long = 500;
+        $image->resize($long, $long, false);
+        return $image->save($newPath, $format, 80);
+    },
+    'l600' => function (\PHPixie\Image\Drivers\Driver\Resource $image, $newPath, $format) {
+        $long = 600;
+        $image->resize($long, $long, false);
+        return $image->save($newPath, $format, 80);
+    },
+    'l700' => function (\PHPixie\Image\Drivers\Driver\Resource $image, $newPath, $format) {
+        $long = 700;
+        $image->resize($long, $long, false);
+        return $image->save($newPath, $format, 80);
+    },
+    'l800' => function (\PHPixie\Image\Drivers\Driver\Resource $image, $newPath, $format) {
+        $long = 800;
+        $image->resize($long, $long, false);
+        return $image->save($newPath, $format, 80);
+    },
     'l1200' => function (\PHPixie\Image\Drivers\Driver\Resource $image, $newPath, $format) {
         $long = 1200;
         $image->resize($long, $long, false);
         return $image->save($newPath, $format, 80);
+    },
+    'orig1200' => function (\PHPixie\Image\Drivers\Driver\Resource $image, $newPath, $format) {
+        $long = 1200;
+        $resize = false;
+        if (($image->width()/$long) - 1 <= 0.1) {
+            $width = $image->width();
+            $resize = false;
+        } else {
+            $width = $long;
+            $resize = true;
+        }
+
+        if (($image->height()/$long) - 1 <= 0.1) {
+            $heigth = $image->width();
+            $resize = false;
+        } else {
+            $heigth = $long;
+            $resize = true;
+        }
+        $image->resize($width, $heigth, false);
+        return $image->save($newPath, $format, 100);
     },
 ];
