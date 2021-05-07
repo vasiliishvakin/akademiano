acfc114a204eea57e3bfb2e4c34020b6017bba22
mrdatamapper <mrdatamapper@gmail.com>
2021-05-07 16:17:01 +0800

    new changes

--- src/Container.php
+++ src/Container.php
@@ -11,6 +11,83 @@ class Container extends \Pimple\Container implements ArrayableInterface
 {
     protected $values = [];
 
+    private \Closure $factoriesGetter;
+    private \Closure $rawsGetter;
+    private \Closure $valuesGetter;
+
+    /**
+     * Instantiates the container.
+     *
+     * Objects and parameters can be passed as argument to the constructor.
+     *
+     * @param array $values The parameters or objects
+     */
+    public function __construct(array $values = [])
+    {
+        parent::__construct($values);
+
+
+        $getterFactoriesClosure = \Closure::bind(function (\Pimple\Container $container) {
+            return $container->factories;
+        }, null, \Pimple\Container::class);
+
+        $this->factoriesGetter = function () use ($getterFactoriesClosure) {
+            return $getterFactoriesClosure($this);
+        };
+
+        $getterRawClosure = \Closure::bind(function (\Pimple\Container $container) {
+            return $container->raw;
+        }, null, \Pimple\Container::class);
+
+        $this->rawsGetter = function () use ($getterRawClosure) {
+            return $getterRawClosure($this);
+        };
+
+        $getterValuesClosure = \Closure::bind(function (\Pimple\Container $container) {
+            return $container->values;
+        }, null, \Pimple\Container::class);
+
+        $this->valuesGetter = function () use ($getterValuesClosure) {
+            return $getterValuesClosure($this);
+        };
+    }
+
+    protected function getFactories()
+    {
+        return ($this->factoriesGetter)();
+    }
+
+    protected function getRaws()
+    {
+        return ($this->rawsGetter)();
+    }
+
+
+    protected function getValues()
+    {
+        return ($this->valuesGetter)();
+    }
+
+    protected function getRaw($id)
+    {
+        $raws = $this->getRaws();
+        if (isset($raws[$id])) {
+            return $raws[$id];
+        }
+        $values = $this->getValues();
+        return $values[$id];
+    }
+
+    protected function isFactory($id): bool
+    {
+        $raw = $this->getRaw($id);
+        if (!$raw instanceof \Closure) {
+            return false;
+        }
+        $factories = $this->getFactories();
+        return isset($factories[$raw]);
+    }
+
     public function lazyGet($id)
     {
         return function () use ($id) {
@@ -30,6 +107,9 @@ class Container extends \Pimple\Container implements ArrayableInterface
             throw new UnknownIdentifierException($id);
         }
         if (!isset($this->values[$id])) {
+            if ($this->isFactory($id)) {
+                return $this->prepare(parent::offsetGet($id));
+            }
             $this->values[$id] = $this->prepare(parent::offsetGet($id));
         }
         return $this->values[$id];
